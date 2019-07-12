946ac02c566753decf58d5e9cb53b0e31d2bb2e6
psachan190 <psachan190@gmail.com>
2019-07-12 16:35:35 +0530

    Project start.

---
+++ server.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var connect = require('connect')
+var methodOverride = require('method-override')
+
+//Routes
+var routes = require('./routes/index');
+//var books = require('./routes/books');
+
+var app = express();
+
+//View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
+app.use(methodOverride('_method'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//Use routes
+app.use('/', routes);
+//app.use('/books', books);
+
+//Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+//Error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+app.listen(8081, () => {
+  console.log('App was created by Prashant Sachan and Server is running on 8081 port.');
+});
+module.exports = app;
